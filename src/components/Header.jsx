8bc098547624543881c5fca3915cdd5df9c41804
Dut Kulang <dutandrew78@gmail.com>
2025-09-27 16:48:05 +0200

    Add header component with its styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+const Header  = ()=>{
+    return (
+        <header>
+            <img className="logo" src="https://www.pngplay.com/wp-content/uploads/6/Free-Symbol-Transparent-PNG.png" />
+        
+            <nav>
+                <ul className="navbar-nav">
+                    <li>Software</li>
+                    <li>Videos</li>
+                    <li>Jokes</li>
+                    <li>Movies</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
