1e5928bd6fe1d97b4f8a7d3a7fb127cd2d650e4e
Dut Kulang <dutandrew78@gmail.com>
2025-09-27 17:21:47 +0200

    add item component and it's styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import Header from "./components/Header"
+import { ItemCard } from './components/ItemCard'
 function App() {
 
   return (
     <>
         <Header />
+        <section className='item-container'>
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </section>
     </>
   )
 }
---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,12 @@
+export const ItemCard = ()=>{
+    return (
+        <div className="item">
+        <img className="item-image" src="https://images.pexels.com/photos/305070/pexels-photo-305070.jpeg" alt="" />
+        <div className="item-content">
+            <h3>Car Name</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <button className="view-btn">View ...</button>
+        </div>
+        </div>
+    )
+}
